b87ffe459c944b946a22f0b42babf6003a5dd82b
carlos-xbm <carlos.xbm@outlook.com>
2025-08-12 11:05:33 -0300

    Projeto dio atribuindo celular com POO concluido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,35 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
 
-        public Smartphone(string numero)
+        private string IMEI { get; set; }
+
+        private int Memoria { get; set; }
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
+        }
+        public void NumeroSmartphone()
+        {
+            System.Console.WriteLine($"Numero: {Numero}");
+        }
+        public void NomeModelo()
+        {
+            System.Console.WriteLine($"Modelo: {Modelo}");
+        }
+        public void NumeroIMEI()
+        {
+            System.Console.WriteLine($"IMEI: {IMEI}");
+        }
+        public void QuantidadeMemoria()
+        {
+            System.Console.WriteLine($"Memoria: {Memoria}");
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+System.Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "11221122", memoria: 64);
+nokia.NumeroSmartphone();
+nokia.NomeModelo();
+nokia.NumeroIMEI();
+nokia.QuantidadeMemoria();
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+System.Console.WriteLine("\n");
+
+System.Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone(numero: "789123", modelo: "Modelo 2", imei: "33443344", memoria: 128);
+iphone.NumeroSmartphone();
+iphone.NomeModelo();
+iphone.NumeroIMEI();
+iphone.QuantidadeMemoria();
+iphone.Ligar();
+iphone.InstalarAplicativo("Whatsapp");
